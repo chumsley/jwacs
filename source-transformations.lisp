26b0d830068488393c57969bcced3ef491a25878
chumsley <chumsley@gmail.com>
2005-11-07 20:51:57 -0800

    init-source-transforms
    Added source-transformations.lisp for defining transformations on parse trees.
    This initial version contains only the default functionality (for propagating
    transformations to child elements of structures and lists and returning self
    without modification for atomic types), plus the trivial "hello" transform
    which is used for testing.
    
    darcs-hash:20051108045157-a67ae-6d74eac4650e300d32951d954751ff9674aa2577

---
+++ source-transformations.lisp
@@ -0,0 +1,64 @@
+;; source-transformations
+(in-package :jwacs)
+
+;;;;; Utility functions
+;;TODO Figure out an entry for SBCL
+(defun structure-slots (object)
+  #+openmcl
+  (let* ((sd (gethash (class-name (class-of object)) ccl::%defstructs%))
+	 (slots (if sd (ccl::sd-slots sd))))
+    (mapcar #'car (if (symbolp (caar slots)) slots (cdr slots))))
+  #+cmu
+  (mapcar #'pcl:slot-definition-name (pcl:class-slots (class-of object)))
+  #+lispworks
+  (structure:structure-class-slot-names (class-of object))
+  #+allegro
+  (mapcar #'mop:slot-definition-name (mop:class-slots (class-of object))))
+
+(defparameter *copy-functions* (make-hash-table :test 'eq)
+  "Map from structure-type symbol to copy-function symbol")
+
+(defun get-copier (struct-object)
+  "Accept a structure object and return the (likely) name of its copy-function"
+  (let* ((name (type-of struct-object))
+         (copy-fn (gethash name *copy-functions*)))
+    (if (null copy-fn)
+      (setf (gethash name *copy-functions*) (intern (format nil "COPY-~A" name)))
+      copy-fn)))
+
+;;;;; Default transformation behaviour
+
+(defgeneric transform (xform elm)
+  (:documentation
+   "Accepts a transformation name (symbol) and a source element, and returns a new
+source element that has been transformed in some way.  Methods should /not/ perform
+destructive updates on the provided source-element."))
+
+;; The default behaviour for any transformation is to do nothing
+(defmethod transform (xform elm)
+  elm)
+
+;; The default behaviour for any transformation on a source-element that has children
+;; is to return a new source-element whose children have been transformed.
+(defmethod transform (xform (elm structure-object))
+  (let ((fresh-elm (funcall (get-copier elm) elm)))
+    (dolist (slot (structure-slots elm))
+      (setf (slot-value fresh-elm slot)
+            (transform xform (slot-value elm slot))))
+    fresh-elm))        
+
+(defmethod transform (xform (elm list))
+  (mapcar (lambda (arg)
+            (transform xform arg))
+          elm))
+
+;;;;; Unit tests
+(defmethod transform ((xform (eql 'hello)) (elm string))
+    "hello there!")
+
+(defun test-transform ()
+  (and
+   (equal (continue-label (transform 'hello (make-continue :label "go away!")))
+          "hello there!")
+   (equal (transform 'hello '("string 1" symbol ("string 2")))
+                     '("hello there!" symbol ("hello there!")))))
