2f4e03a6a875817ff705ff59907bd027e0b35c1d
James Wright <chumsley@gmail.com>
2005-11-10 23:59:01 -0800

    add-tests-2
    Add more new unit tests
    
    darcs-hash:20051111075901-53808-40a6b6ad618d2845ce9b4795b9303ec5d096d57b

--- tests/test-lexer.lisp
+++ tests/test-lexer.lisp
@@ -2,39 +2,47 @@
 ;;;
 ;;; Unit tests for the Javascript lexer.
 (in-package :jwacs-tests)
+(import '(jwacs::regexp-re
+          jwacs::make-javascript-lexer))
 
+;;;; Helper functions
 (defun non-null (x)
   "Return T if X is non-null.  This is a convenience function that
    frees us from the necessity of having to know exactly which non-null
    value a test should expect."
   (not (null x)))
 
-(deftest regexp-re/1 :note (lexer regexp)
-  (non-null (scan jwacs::regexp-re "/hello/"))
+;;;; Test categories
+(defnote lexer "tests for the lexer")
+(defnote regexp "tests for individual regular expressions")
+
+;;;; Tests
+(deftest regexp-re/1 :notes (lexer regexp)
+  (non-null (scan regexp-re "/hello/"))
   t)
 
-(deftest regexp-re/2 :note (lexer regexp)
-  (non-null (scan jwacs::regexp-re "/.\\n/"))
+(deftest regexp-re/2 :notes (lexer regexp)
+  (non-null (scan regexp-re "/.\\n/"))
   t)
 
-(deftest regexp-re/3 :note (lexer regexp)
-  (non-null (scan jwacs::regexp-re "/(this)/g"))
+(deftest regexp-re/3 :notes (lexer regexp)
+  (non-null (scan regexp-re "/(this)/g"))
   t)
 
-(deftest regexp-re/4 :note (lexer regexp)
-  (non-null (scan jwacs::regexp-re "/(this)/gi"))
+(deftest regexp-re/4 :notes (lexer regexp)
+  (non-null (scan regexp-re "/(this)/gi"))
   t)
 
-(deftest regexp-re/5 :note (lexer regexp)
-  (scan jwacs::regexp-re "\"hi\"")
+(deftest regexp-re/5 :notes (lexer regexp)
+  (scan regexp-re "\"hi\"")
   nil)
 
-(deftest regexp-re/6 :note (lexer regexp)
-  (scan jwacs::regexp-re "/\"hi\"")
+(deftest regexp-re/6 :notes (lexer regexp)
+  (scan regexp-re "/\"hi\"")
   nil)
 
 
-(deftest lexer/1 :note lexer
+(deftest lexer/1 :notes lexer
   (let ((js-string-1 "/* test string */
                       function (f)
                       {
@@ -42,42 +50,42 @@
                         var m = 010;
                         doStuff('stuff', \"nonsense\", 0xff, 45.0, f(m));
                       }"))
-    (loop with l = (jwacs::make-javascript-lexer js-string-1)
+    (loop with l = (make-javascript-lexer js-string-1)
           for x = (multiple-value-list (funcall l))
           while (not (eq (car x) :eoi))
           collect x))
-  ((:FUNCTION "function")
-   (:LEFT-PAREN "(")
-   (:IDENTIFIER "f")
-   (:RIGHT-PAREN ")")
-   (:LEFT-CURLY "{")
-   (:VAR "var")
-   (:IDENTIFIER "m")
-   (:EQUALS "=")
-   (:NUMBER 8)
-   (:SEMICOLON ";")
-   (:IDENTIFIER "doStuff")
-   (:LEFT-PAREN "(")
-   (:STRING-LITERAL "stuff")
-   (:COMMA ",")
-   (:STRING-LITERAL "nonsense")
-   (:COMMA ",")
-   (:NUMBER 255)
-   (:COMMA ",")
-   (:NUMBER 45.0)
-   (:COMMA ",")
-   (:IDENTIFIER "f")
-   (:LEFT-PAREN "(")
-   (:IDENTIFIER "m")
-   (:RIGHT-PAREN ")")
-   (:RIGHT-PAREN ")")
-   (:SEMICOLON ";")
-   (:RIGHT-CURLY "}")))
+  ((:function "function")
+   (:left-paren "(")
+   (:identifier "f")
+   (:right-paren ")")
+   (:left-curly "{")
+   (:var "var")
+   (:identifier "m")
+   (:equals "=")
+   (:number 8)
+   (:semicolon ";")
+   (:identifier "doStuff")
+   (:left-paren "(")
+   (:string-literal "stuff")
+   (:comma ",")
+   (:string-literal "nonsense")
+   (:comma ",")
+   (:number 255)
+   (:comma ",")
+   (:number 45.0)
+   (:comma ",")
+   (:identifier "f")
+   (:left-paren "(")
+   (:identifier "m")
+   (:right-paren ")")
+   (:right-paren ")")
+   (:semicolon ";")
+   (:right-curly "}")))
     
-(deftest lexer/2 :note lexer
+(deftest lexer/2 :notes lexer
   (let ((js-string-2 "var re1 = /hello/g;
                      var re2 = /hello\\/goodbye/ig;"))
-    (loop with l = (jwacs::make-javascript-lexer js-string-2)
+    (loop with l = (make-javascript-lexer js-string-2)
           for x = (multiple-value-list (funcall l))
           while (not (eq (car x) :eoi))
           collect x))
---
+++ tests/test-pretty-print.lisp
@@ -0,0 +1,269 @@
+;;;; test-pretty-print.lisp
+;;;
+;;; Tests for the pretty-printer.
+
+(in-package :jwacs-tests)
+(import '(jwacs::make-source-element
+          jwacs::make-special-value
+          jwacs::make-identifier
+          jwacs::make-numeric-literal
+          jwacs::make-string-literal
+          jwacs::make-array-literal
+          jwacs::make-object-literal
+          jwacs::make-new-expr
+          jwacs::make-fn-call
+          jwacs::make-property-access
+          jwacs::make-unary-operator
+          jwacs::make-binary-operator
+          jwacs::make-conditional
+          jwacs::make-comma-expr
+          jwacs::make-var-decl-stmt
+          jwacs::make-var-decl
+          jwacs::make-block
+          jwacs::make-if
+          jwacs::make-do
+          jwacs::make-while
+          jwacs::make-for
+          jwacs::make-for-in
+          jwacs::make-continue
+          jwacs::make-break
+          jwacs::make-return
+          jwacs::make-with
+          jwacs::make-switch
+          jwacs::make-case
+          jwacs::make-default
+          jwacs::make-label
+          jwacs::make-throw
+          jwacs::make-try
+          jwacs::make-catch-clause
+          jwacs::make-finally-clause
+          jwacs::make-function-decl
+          jwacs::make-function-expression
+
+          jwacs::pretty-print
+          jwacs::with-indent
+          jwacs::*indent*))
+
+;;;; Helper functions
+(defun pretty-string (elm)
+  "Pretty-print ELM to a string value instead of a stream."
+  (with-output-to-string (s)
+    (pretty-print elm s)))
+
+;;;; Test categories
+(defnote pretty-print "tests for the pretty-printer")
+
+;;;; Tests
+(deftest pretty-print-special-value/1 :notes pretty-print
+  (pretty-string (make-special-value :symbol :this))
+  "this")
+
+(deftest pretty-print-special-value/2 :notes pretty-print
+  (pretty-string (make-special-value :symbol :true))
+  "true")
+  
+;;; Helpful constants
+(defconstant foo-id (make-identifier :name "foo"))
+(defconstant bar-id (make-identifier :name "bar"))
+(defconstant baz-id (make-identifier :name "baz"))
+
+;;;; TODO Legacy tests (to be converted and then deleted)
+#|
+(defun test-pretty-printer ()
+  (flet ((check (elm string)
+           (let ((output (with-output-to-string (s)
+                    (pretty-print elm s))))
+             (if (equal string output)
+               t
+               (format t "~&Mismatch: ~S --> ~S" elm output)))))
+                  
+    (let ((foo-id (make-identifier :name "foo")) ; Some handy identifiers for using as subexpressions
+          (bar-id (make-identifier :name "bar"))
+          (baz-id (make-identifier :name "baz"))
+          (*indent* 0)) ; Ensure known values for indentation
+      (and
+       
+       (pretty-string (make-special-value :symbol :false)
+              "false")
+       (pretty-string (make-special-value :symbol :null)
+              "null")
+       (pretty-string (make-special-value :symbol :undefined)
+              "undefined")
+       (pretty-string (make-unary-operator :op-symbol :void :arg foo-id)
+              "void foo")
+       (pretty-string (make-unary-operator :op-symbol :delete :arg foo-id)
+              "delete foo")
+       (pretty-string (make-unary-operator :op-symbol :post-incr :arg foo-id)
+              "foo++")
+       (pretty-string (make-unary-operator :op-symbol :post-decr :arg foo-id)
+              "foo--")
+       (pretty-string (make-unary-operator :op-symbol :pre-incr :arg foo-id)
+              "++foo")
+       (pretty-string (make-unary-operator :op-symbol :pre-decr :arg foo-id)
+              "--foo")
+       (pretty-string (make-unary-operator :op-symbol :unary-plus :arg foo-id)
+              "+foo")
+       (pretty-string (make-unary-operator :op-symbol :unary-minus :arg foo-id)
+              "-foo")
+       (pretty-string (make-binary-operator :op-symbol :add :left-arg foo-id :right-arg bar-id)
+              "foo + bar")
+       (pretty-string (make-binary-operator :op-symbol :lshift :left-arg foo-id :right-arg bar-id)
+              "foo << bar")
+       (pretty-string (make-binary-operator :op-symbol :instanceof :left-arg foo-id :right-arg bar-id)
+              "foo instanceof bar")
+       (pretty-string (make-conditional :condition foo-id :true-arg bar-id :false-arg (make-special-value :symbol :null))
+              "(foo) ? (bar) : (null)")
+       (pretty-string (make-numeric-literal :value 50)
+              "50")
+       (pretty-string (make-var-decl-stmt :var-decls 
+                                  (list (make-var-decl :name "x")
+                                        (make-var-decl :name "y" :initializer (make-numeric-literal :value 50))))
+              "var x, y = 50")
+       (pretty-string (make-block :statements
+                            (list (make-var-decl-stmt :var-decls (list (make-var-decl :name "x" :initializer (make-numeric-literal :value 55))))
+                                  (make-binary-operator :op-symbol :times-equals :left-arg (make-identifier :name "x") :right-arg foo-id)))
+              "{
+  var x = 55;
+  x *= foo;
+}")
+       (with-indent
+         (pretty-string (make-block :statements
+                            (list (make-var-decl-stmt :var-decls (list (make-var-decl :name "x" :initializer (make-numeric-literal :value 55))))
+                                  (make-binary-operator :op-symbol :times-equals :left-arg (make-identifier :name "x") :right-arg foo-id)))
+                "  {
+    var x = 55;
+    x *= foo;
+  }"))
+       (pretty-string (make-if :condition foo-id :then-statement bar-id)
+              "if(foo)
+  bar")
+       (pretty-string (make-if :condition foo-id :then-statement bar-id :else-statement baz-id)
+              "if(foo)
+  bar;
+else
+  baz")
+       (pretty-string (make-if :condition foo-id :then-statement (make-block :statements (list bar-id)))
+              "if(foo)
+{
+  bar;
+}")
+       (pretty-string (make-if :condition foo-id :then-statement (make-block :statements (list bar-id)) :else-statement baz-id)
+              "if(foo)
+{
+  bar;
+}
+else
+  baz")
+       (pretty-string (make-if :condition foo-id :then-statement bar-id :else-statement (make-block :statements (list baz-id)))
+              "if(foo)
+  bar;
+else
+{
+  baz;
+}")
+       (pretty-string (make-do :condition (make-binary-operator :op-symbol :greater-than
+                                                        :left-arg foo-id
+                                                        :right-arg (make-numeric-literal :value 55.0))
+                       :body (make-block :statements
+                                         (list (make-unary-operator :op-symbol :post-incr :arg foo-id))))
+              "do
+{
+  foo++;
+}
+while(foo > 55.0)")
+       (pretty-string (make-while :condition (make-unary-operator :op-symbol :typeof :arg foo-id)
+                              :body (make-unary-operator :op-symbol :delete :arg foo-id))
+              "while(typeof foo)
+  delete foo")
+       (pretty-string (make-while :condition (make-unary-operator :op-symbol :typeof :arg foo-id)
+                              :body (make-block :statements (list (make-unary-operator :op-symbol :delete :arg foo-id))))
+              "while(typeof foo)
+{
+  delete foo;
+}")
+       (pretty-string (make-for :body (make-block))
+              "for(; ; )
+{
+}")
+       (pretty-string (make-for :initializer (make-var-decl-stmt :var-decls (list (make-var-decl :name "foo" :initializer bar-id)))
+                        :condition (make-binary-operator :op-symbol :not-equals :left-arg foo-id :right-arg baz-id)
+                        :step (make-unary-operator :op-symbol :pre-incr :arg foo-id)
+                        :body (make-block :statements (list (make-unary-operator :op-symbol :post-decr :arg foo-id))))
+              "for(var foo = bar; foo != baz; ++foo)
+{
+  foo--;
+}")
+       (pretty-string (make-for-in :binding (make-var-decl-stmt :var-decls (list (make-var-decl :name "foo")))
+                           :collection bar-id
+                           :body (make-fn-call :fn baz-id :args (list foo-id bar-id)))
+              "for(var foo in bar)
+  baz(foo, bar)")
+
+       (pretty-string (make-for-in :binding foo-id
+                           :collection bar-id
+                           :body (make-fn-call :fn baz-id :args (list foo-id bar-id)))
+              "for(foo in bar)
+  baz(foo, bar)")
+
+       (pretty-string (make-switch :value foo-id :clauses
+                 (list
+                  (make-case :label (make-numeric-literal :value 10)
+                             :body (list
+                                    (make-fn-call :fn bar-id :args (list (make-numeric-literal :value 1)))
+                                    (make-fn-call :fn baz-id :args (list foo-id))
+                                    (make-break)))
+                  (make-case :label (make-numeric-literal :value 20))
+                  (make-case :label (make-numeric-literal :value 30)
+                             :body (list
+                                    (make-fn-call :fn bar-id :args (list (make-numeric-literal :value 3)))))
+                  (make-default :body (list
+                                       (make-return :arg foo-id)))))
+              "switch(foo)
+{
+case 10:
+  bar(1);
+  baz(foo);
+  break;
+case 20:
+case 30:
+  bar(3);
+default:
+  return foo;
+}")
+       (pretty-string (make-with :scope-object foo-id :body (make-block :statements (list bar-id)))
+              "with(foo)
+{
+  bar;
+}")
+       (pretty-string (make-label :name "fhwqgads" :statement (make-fn-call :fn foo-id :args (list bar-id baz-id)))
+              "fhwqgads:
+foo(bar, baz)")
+
+       (pretty-string (make-try :body (make-block :statements (list (make-fn-call :fn foo-id :args (list bar-id))))
+                        :catch-clause (make-catch-clause :binding "e" :body (make-block :statements (list (make-fn-call :fn foo-id :args (list (make-identifier :name "e"))))))
+                        :finally-clause (make-finally-clause :body (make-block :statements (list (make-fn-call :fn baz-id) (make-unary-operator :op-symbol :delete :arg foo-id)))))
+              "try
+{
+  foo(bar);
+}
+catch(e)
+{
+  foo(e);
+}
+finally
+{
+  baz();
+  delete foo;
+}")
+       (pretty-string
+        (make-function-decl :name "yarb" :parameters '("x" "y") :body (list (make-fn-call :fn foo-id) (make-fn-call :fn bar-id) (make-fn-call :fn baz-id)))
+        "function yarb(x, y)
+{
+  foo();
+  bar();
+  baz();
+}")
+
+       ;; HERE TODO Add test case(s) for function expressions (at least one for single-line and one for multi-line)
+       ))))
+|#
