804d3308b4ba12ea4d596df2c394f10c0c5d4d3f
greg <greg@smolyn.org>
2005-11-11 14:55:31 -0800

    yacc-compat
    Here's the scoop on the changes:
     - added parse-javascript-yacc.lisp, which contains a macro definition
       for defparser that will convert Lispworks' defparser syntax into
       CL-YACC's define-parser
     - I added a defun for parse, however that will get clobbered by the one
       in parse-javascrip.lisp.  I haven't made a big effort to encorporate
       a seamlesse experience just yet. ALPHA QUALITY! :)
     - added some reference examples of javascript grammars for other parsgens
       in the reference/ dir
     - changed the js-source-model.lisp to force the struct names to be symbols
       with ":".  SBCL got angry with the un-colonified versions.
     - Added CL-YACC directly to the repo (yacc.lisp).  This seems to be the
       accepted method of distribution/inclusion (there is no available CL-YACC
       package)
     And with that, enjoy. :)
     PS. None of this has been added to the package defintion as of yet.
    
    darcs-hash:20051111225531-2e96c-7e353880a5f7f8064a7baed4c97e47ab13c36806

--- js-source-model.lisp
+++ js-source-model.lisp
@@ -5,139 +5,139 @@
 
 (in-package :jwacs)
 
-(defstruct source-element
+(defstruct :source-element
   "A common base type for all source elements")
 
-(defstruct (special-value (:include source-element))
+(defstruct (:special-value (:include source-element))
   symbol)
 
-(defstruct (identifier (:include source-element))
+(defstruct (:identifier (:include source-element))
   name)
 
-(defstruct (numeric-literal (:include source-element))
+(defstruct (:numeric-literal (:include source-element))
   value)
 
-(defstruct (string-literal (:include source-element))
+(defstruct (:string-literal (:include source-element))
   value)
 
-(defstruct (array-literal (:include source-element))
+(defstruct (:array-literal (:include source-element))
   elements)
 
-(defstruct (object-literal (:include source-element))
+(defstruct (:object-literal (:include source-element))
   properties)  ; List of (PROPERTY-NAME . PROPERTY-VALUE)
 
-(defstruct (new-expr (:include source-element))
+(defstruct (:new-expr (:include source-element))
   object-name
   args)
 
-(defstruct (fn-call (:include source-element))
+(defstruct (:fn-call (:include source-element))
   fn
   args)
 
-(defstruct (property-access (:include source-element))
+(defstruct (:property-access (:include source-element))
   target
   field)
 
-(defstruct (unary-operator (:include source-element))
+(defstruct (:unary-operator (:include source-element))
   op-symbol
   arg)
 
-(defstruct (binary-operator (:include source-element))
+(defstruct (:binary-operator (:include source-element))
   op-symbol
   left-arg
   right-arg)
     
-(defstruct (conditional (:include source-element))
+(defstruct (:conditional (:include source-element))
   condition
   true-arg
   false-arg)
 
-(defstruct (comma-expr (:include source-element))
+(defstruct (:comma-expr (:include source-element))
   exprs)
 
-(defstruct (var-decl-stmt (:include source-element))
+(defstruct (:var-decl-stmt (:include source-element))
   var-decls)
 
-(defstruct (var-decl (:include source-element))
+(defstruct (:var-decl (:include source-element))
   name
   initializer)
 
-(defstruct (block (:include source-element))
+(defstruct (:block (:include source-element))
   statements)
 
-(defstruct (if (:include source-element))
+(defstruct (:if (:include source-element))
   condition
   then-statement
   else-statement)
 
-(defstruct (do (:include source-element))
+(defstruct (:do (:include source-element))
   condition
   body)
 
-(defstruct (while (:include source-element))
+(defstruct (:while (:include source-element))
   condition
   body)
 
-(defstruct (for (:include source-element))
+(defstruct (:for (:include source-element))
   initializer
   condition
   step
   body)
 
-(defstruct (for-in (:include source-element))
+(defstruct (:for-in (:include source-element))
   binding
   collection
   body)
 
-(defstruct (continue (:include source-element))
+(defstruct (:continue (:include source-element))
   label)
 
-(defstruct (break (:include source-element))
+(defstruct (:break (:include source-element))
   label)
 
-(defstruct (return (:include source-element))
+(defstruct (:return (:include source-element))
   arg)
 
-(defstruct (with (:include source-element))
+(defstruct (:with (:include source-element))
   scope-object
   body)
 
-(defstruct (switch (:include source-element))
+(defstruct (:switch (:include source-element))
   value
   clauses)
 
-(defstruct (case (:include source-element))
+(defstruct (:case (:include source-element))
   label
   body)
 
-(defstruct (default (:include source-element))
+(defstruct (:default (:include source-element))
   body)
 
-(defstruct (label (:include source-element))
+(defstruct (:label (:include source-element))
   name
   statement)
 
-(defstruct (throw (:include source-element))
+(defstruct (:throw (:include source-element))
   value)
 
-(defstruct (try (:include source-element))
+(defstruct (:try (:include source-element))
   body
   catch-clause
   finally-clause)
 
-(defstruct (catch-clause (:include source-element))
+(defstruct (:catch-clause (:include source-element))
   binding
   body)
 
-(defstruct (finally-clause (:include source-element))
+(defstruct (:finally-clause (:include source-element))
   body)
 
-(defstruct (function-decl (:include source-element))
+(defstruct (:function-decl (:include source-element))
   name
   parameters
   body)
 
-(defstruct (function-expression (:include source-element))
+(defstruct (:function-expression (:include source-element))
   name
   parameters
   body)
---
+++ parse-javascript-yacc.lisp
@@ -0,0 +1,76 @@
+(in-package :jwacs)
+
+(defun expand-hashtable-to-values (hashtable)
+  "Returns a list of all the values stored in a hashtable."
+  (let ((valuelist '()))
+    (maphash #'(lambda (k v) 
+		 (setf valuelist (cons v valuelist)))
+	     hashtable)
+    valuelist))
+
+; need to collect productions 
+
+(defmacro defparser (parser-name starting-production &body productions)
+  "This macro emulates the Lispworks parsergenerator's defparser macro, but instead creates output
+   for CL-YACC"
+  (let* ((starting-point (first starting-production))
+	 (starting-symbol (first starting-point))
+	 (header `(define-parser ,parser-name
+		   (:start-symbol ,starting-symbol)
+		   (:terminals ,(expand-hashtable-to-values *tokens-to-symbols* ))
+		   (:precedence nil)
+		   ,starting-point)))
+    (append header (generate-productions productions))))
+
+; here we turn
+;  ((primary-expression object-literal) $1)
+;   into 
+;  (primary-expression
+;      (object-literal #'(lambda (&rest expr) (nth 0 expr))))
+;
+; and
+;
+; ((literal :number) (make-numeric-literal :value $1))
+;  into
+; (literal
+;   (:number #'(lambda (&rest expr) (make-numeric-literal :value (nth 0 expr)))))
+
+
+
+(defun generate-productions (productions)
+  "Used by defparser macro. Take the lispworks list of productions and convert them into
+   CL-YACC versions"
+  (let* ((production-map (make-hash-table)))
+    (dolist (production productions)lisp convert int to char
+      (let* ((rule (nth 0 production))
+	     (action (replace-dollar-signs (nth 1 production)))
+	     (rule-name (first rule))
+	     (rule-terminals (rest rule)))
+	(setf (gethash rule-name production-map) 
+	      (cons (append rule-terminals `(#'(lambda (&rest expr) ,action)))
+		      (gethash rule-name production-map)))))
+    (let* ((output '()))
+      (maphash #'(lambda (k v) 
+		   (setf output (cons (append (list k) v) output)))
+	       production-map)
+      output)))
+    
+
+; go through every symbol
+
+(defun replace-dollar-signs (item)
+  "Takes a symbol or a list and replaces $n with (nth n-1 expr)"
+  (cond ((null item) nil)
+	((not (listp item))
+	 (replace-dollar-sign-in-symbol item))
+	(t (cons (replace-dollar-signs (car item))
+		   (replace-dollar-signs (cdr item))))))
+
+(defun replace-dollar-sign-in-symbol (sym)
+  "Replace $n in a symbol with (nth n-1 expr)"
+  (if (not (eq (char (symbol-name sym) 0) #\$))
+      sym
+      `(nth ,(- (char-int (char (symbol-name sym) 1)) (char-int #\1))expr)))
+
+(defun parse (str)
+  (parse-with-lexer (make-javascript-lexer str) javascript-script))
