b153c9cd7341853aa2e15c56a83a75871d10ae35
James Wright <chumsley@gmail.com>
2006-01-03 21:08:55 -0800

    comment-loop-tests
    The current loop transformation tests don't reflect the new notion of what the
    loop transformation should look like, so I'm commenting them out before we
    replace them with the actual tests.
    
    darcs-hash:20060104050855-53808-075e47b93e5476166c2789908a0035d50a122d1d

--- tests/test-loop-transformation.lisp
+++ tests/test-loop-transformation.lisp
@@ -2,160 +2,162 @@
 
 (defnote loop-to-function "tests for loop-to-function source transformation")
 
-(deftest loop-to-function/while/1 :notes loop-to-function
-  (with-fresh-genvar
-    (transform 'loop-to-function
-               (parse "x=0; while(x<4) { foo(); x++; }")))
-  #.(parse "x=0; 
-            {
-            var JW0 = function JW1() {
-              if(x<4) {
-                foo();
-                x++;
-                JW1();
-              }
-            };         
-           JW0();
-           }"))
+;;XXX These tests reflect the old notion of what loop translations should look like,
+;;     not the spanky new notion, so I'm commenting them out.
+;(deftest loop-to-function/while/1 :notes loop-to-function
+;  (with-fresh-genvar
+;    (transform 'loop-to-function
+;               (parse "x=0; while(x<4) { foo(); x++; }")))
+;  #.(parse "x=0; 
+;            {
+;            var JW0 = function JW1() {
+;              if(x<4) {
+;                foo();
+;                x++;
+;                JW1();
+;              }
+;            };         
+;           JW0();
+;           }"))
 
                   
-(deftest loop-to-function/do-while/1 :notes loop-to-function
- (with-fresh-genvar
-   (transform 'loop-to-function
-	      (parse "x=0; do { foo(); x++; } while(x<4);")))
- #.(parse "x=0;
-           {
-           var JW0 = function JW1() {
-             foo();
-             x++;
-             if(x<4) {
-               JW1();
-             }
-           };
-           JW0();
-           }"))
+;(deftest loop-to-function/do-while/1 :notes loop-to-function
+; (with-fresh-genvar
+;   (transform 'loop-to-function
+;	      (parse "x=0; do { foo(); x++; } while(x<4);")))
+; #.(parse "x=0;
+;           {
+;           var JW0 = function JW1() {
+;             foo();
+;             x++;
+;             if(x<4) {
+;               JW1();
+;             }
+;           };
+;           JW0();
+;           }"))
 
-(deftest loop-to-function/for/1 :notes loop-to-function
-  (with-fresh-genvar
-    (transform 'loop-to-function
-               (parse "for(x=0; x<4; x++) { foo(); }")))
-  #.(parse "{
-             x=0;           
-           var JW0 = function JW1() {
-              if(x<4) {
-               foo();
-               x++;
-               JW1();
-              }
-           };
-           JW0();           
-         }"))
+;(deftest loop-to-function/for/1 :notes loop-to-function
+;  (with-fresh-genvar
+;    (transform 'loop-to-function
+;               (parse "for(x=0; x<4; x++) { foo(); }")))
+;  #.(parse "{
+;             x=0;           
+;           var JW0 = function JW1() {
+;              if(x<4) {
+;               foo();
+;               x++;
+;               JW1();
+;              }
+;           };
+;           JW0();           
+;         }"))
 
-(deftest loop-to-function/for-in/1 :notes loop-to-function
- (with-fresh-genvar
-   (transform 'loop-to-function
-	      (parse "for(var x in obj) 
-                      {
-                        foo();
-                      }")))
- #.(parse " {
-  var JW0 = new Array;
-  var JW1 = 0;
-  for(var JW2 in obj)
-  {
-    JW0[JW1++] = JW2;
-  }
-  var JW3 = function JW4(JW5) { if(JW5 < JW0.length)
-  {
-    var x = JW0[JW5++];
+;(deftest loop-to-function/for-in/1 :notes loop-to-function
+; (with-fresh-genvar
+;   (transform 'loop-to-function
+;	      (parse "for(var x in obj) 
+;                      {
+;                        foo();
+;                      }")))
+; #.(parse " {
+;  var JW0 = new Array;
+;  var JW1 = 0;
+;  for(var JW2 in obj)
+;  {
+;    JW0[JW1++] = JW2;
+;  }
+;  var JW3 = function JW4(JW5) { if(JW5 < JW0.length)
+;  {
+;    var x = JW0[JW5++];
     
-    foo();
+;    foo();
     
-    JW4(JW5);
-  } };
-  JW3(0);
-}"))
+;    JW4(JW5);
+;  } };
+;  JW3(0);
+;}"))
 
-(deftest loop-to-function/break/1 :notes loop-to-function
-  (with-fresh-genvar
-    (transform 'loop-to-function
-               (parse "x=0;
-                       while(x<4)
-                       {
-                         var y = foo(x++);
-                         if(y == null)
-                           break;
-                       }")))
-  #.(parse "x=0;
-            {
-              var JW0 = function JW1() {
-                if(x < 4)
-                {
-                  var y = foo(x++);
-                  if(y == null)
-                    return;
-                }
-              };
-            }"))
+;(deftest loop-to-function/break/1 :notes loop-to-function
+;  (with-fresh-genvar
+;    (transform 'loop-to-function
+;               (parse "x=0;
+;                       while(x<4)
+;                       {
+;                         var y = foo(x++);
+;                         if(y == null)
+;                           break;
+;                       }")))
+;  #.(parse "x=0;
+;            {
+;              var JW0 = function JW1() {
+;                if(x < 4)
+;                {
+;                  var y = foo(x++);
+;                  if(y == null)
+;                    return;
+;                }
+;              };
+;            }"))
                   
-(deftest loop-to-function/continue/1 :notes loop-to-function
-  (with-fresh-genvar
-    (transform 'loop-to-function
-               (parse "x=0;
-                       while(x<4)
-                       {
-                         var y = foo(x++);
-                         if(y == null)
-                           continue;
-                       }")))
-  #.(parse "x=0;
-            {
-              var JW0 = function JW1() {
-                if(x < 4)
-                {
-                  var y = foo(x++);
-                  if(y == null)
-                    JW1();
-                }
-              };
-            }"))
+;(deftest loop-to-function/continue/1 :notes loop-to-function
+;  (with-fresh-genvar
+;    (transform 'loop-to-function
+;               (parse "x=0;
+;                       while(x<4)
+;                       {
+;                         var y = foo(x++);
+;                         if(y == null)
+;                           continue;
+;                       }")))
+;  #.(parse "x=0;
+;            {
+;              var JW0 = function JW1() {
+;                if(x < 4)
+;                {
+;                  var y = foo(x++);
+;                  if(y == null)
+;                    JW1();
+;                }
+;              };
+;            }"))
 
-(deftest loop-to-function/continue/2 :notes loop-to-function
-  (with-fresh-genvar
-    (transform 'loop-to-function
-               (parse "for(x=0; x<4; x++) { if(bar()) continue; foo(); }")))
-  #.(parse "{
-              x=0;           
-              var JW0 = function JW1() {
-                if(x<4) {
-                  if(bar()) {
-                    x++;
-                    JW1();
-                  }
-                  foo();
-                  x++;
-                  JW1();
-                }
-              };
-              JW0();           
-            }"))
+;(deftest loop-to-function/continue/2 :notes loop-to-function
+;  (with-fresh-genvar
+;    (transform 'loop-to-function
+;               (parse "for(x=0; x<4; x++) { if(bar()) continue; foo(); }")))
+;  #.(parse "{
+;              x=0;           
+;              var JW0 = function JW1() {
+;                if(x<4) {
+;                  if(bar()) {
+;                    x++;
+;                    JW1();
+;                  }
+;                  foo();
+;                  x++;
+;                  JW1();
+;                }
+;              };
+;              JW0();           
+;            }"))
 
-(deftest loop-to-function/continue/3 :notes loop-to-function
-  (with-fresh-genvar
-    (transform 'loop-to-function
-               (parse "x=0; do { if(foo()) continue; x++; } while(x<4);")))
-  #.(parse "x=0;
-            {
-              var JW0 = function JW1() {
-                if(foo())
-                  if(x<4)
-                    JW1();
-                  else
-                    return;
-                x++;
-                if(x<4) {
-                  JW1();
-                }
-              };
-              JW0();
-            }"))
+;(deftest loop-to-function/continue/3 :notes loop-to-function
+;  (with-fresh-genvar
+;    (transform 'loop-to-function
+;               (parse "x=0; do { if(foo()) continue; x++; } while(x<4);")))
+;  #.(parse "x=0;
+;            {
+;              var JW0 = function JW1() {
+;                if(foo())
+;                  if(x<4)
+;                    JW1();
+;                  else
+;                    return;
+;                x++;
+;                if(x<4) {
+;                  JW1();
+;                }
+;              };
+;              JW0();
+;            }"))
