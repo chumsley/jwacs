ebcef13e06c0571b4f56c567f759e144fcd25a54
James Wright <chumsley@gmail.com>
2006-08-03 18:36:28 -0700

    delivery-improvements
    The delivery script now gets its build parameters from globals defined in the
    system package, rather than defining its own.
    
    darcs-hash:20060804013628-53808-1a030c802ced61f249c61971d0b265d31893b1ea

--- deliver-sbcl.lisp
+++ deliver-sbcl.lisp
@@ -5,10 +5,6 @@
 (require :jwacs)
 (in-package :jwacs)
 
-(defparameter *executable-name*
-  #+win32 "jwacs.exe"
-  #-win32 "jwacs"
-  "The name of the executable to create")
-
-(sb-ext:save-lisp-and-die *executable-name* :executable t
+(sb-ext:save-lisp-and-die jw-system:*executable-name*
+                          :executable t
                           :toplevel #'main)
